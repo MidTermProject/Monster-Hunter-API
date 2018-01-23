850065f89e83bf1369ce57a0446cdbed2d30b3e5
Dustin Mundy <Dustin.A.Mundy@gmail.com>
2018-01-23 11:59:36 -0800

    Added OneBlade, OneMaterial and OneLocation models which inherit from Blade, Material and Location models respectively. These models add data that is not stored with their parent models in the database for redundancy reasons.

---
+++ MonsterHunterAPI/Models/OneLocation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MonsterHunterAPI.Models
+{
+    public class OneLocation : Location
+    {
+        public int DropRate { get; set; }
+        public string Action { get; set; }
+    }
+}
