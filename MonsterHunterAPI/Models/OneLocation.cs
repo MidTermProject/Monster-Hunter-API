af4b8a8fd54eb8f931269b4c1484b73a0ab765c1
Dustin Mundy <Dustin.A.Mundy@gmail.com>
2018-01-23 11:42:17 -0800

    Added some models

---
+++ MonsterHunterAPI/Models/OneLocation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MonsterHunterAPI.Models
+{
+    public class OneLocation : Location
+    {
+        public string Action { get; set; }
+        public int Droprate { get; set; }
+    }
+}
