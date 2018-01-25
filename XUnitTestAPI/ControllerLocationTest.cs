2275094dea4c2dc83786cce9abe13554273dc35e
Dustin Mundy <Dustin.A.Mundy@gmail.com>
2018-01-24 17:37:51 -0800

    Not working at all yet

---
+++ XUnitTestAPI/ControllerLocationTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MonsterHunterAPI.Controllers;
+using MonsterHunterAPI.Models;
+using MonsterHunterAPI.Data;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace XUnitTestAPI
+{
+    public class ControllerLocationTest
+    {
+        HunterDbContext _context;
+
+        public ControllerLocationTest()
+        {
+            DbContextOptions<HunterDbContext> options = new DbContextOptionsBuilder<HunterDbContext>()
+
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+
+                .Options;
+
+            _context = new HunterDbContext(options);
+
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task TestPostAndGetAsync()
+        {   
+            BladeController _controller = new BladeController(_context);
+
+            Blade blade = new Blade()
+            {
+                Name = "Vergil's Sword"
+            };
+
+            await _controller.Post(blade);
+
+            IEnumerable<Blade> databasedBlade = _controller.Get();
+
+            int afterCount = 5;
+            int beforeCount = 5;
+
+            int difference = afterCount - beforeCount;
+
+            Assert.Equal(1, difference);
+        }
+    }
+}
