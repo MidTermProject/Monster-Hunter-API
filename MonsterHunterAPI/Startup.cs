651f0f30e174b46a3b69cfd80afd7813b984795a
Luay <luayghazwan@yahoo.com>
2018-01-26 10:27:57 -0800

    Added Swagger to swag about our API documentaion

--- MonsterHunterAPI/Startup.cs
+++ MonsterHunterAPI/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
 using MonsterHunterAPI.Data;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace MonsterHunterAPI
 {
@@ -28,11 +29,24 @@ namespace MonsterHunterAPI
             services.AddMvc();
             services.AddDbContext<HunterDbContext>(
                 options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
